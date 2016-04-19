04848fbdbf5651948bca40376102dca19d3db7d6
Ryan <info@rmc-design.co.uk>
2016-04-19 14:25:19 +0100

    Updated media checker function

--- media-checker.js
+++ media-checker.js
@@ -1,14 +1,50 @@
-$(document).ready(function() {
-    // run test on initial page load
-    checkSize();
+//source: http://zerosixthree.se/detecting-media-queries-with-javascript/
 
-    // run test on resize of the window
-    $(window).resize(checkSize);
-});
+'use strict';
+var RMC_media_check = (function (parent, $) {
 
-//Function to the css rule
-function checkSize(){
-    if ($(".sampleClass").css("float") == "none" ){
-        // your code here
+  var MediaQueryListener = function() {
+    this.afterElement = window.getComputedStyle ? window.getComputedStyle(document.body, ':after') : false;
+    this.currentBreakpoint = '';
+    this.lastBreakpoint = '';
+    this.init();
+  };
+
+  MediaQueryListener.prototype = {
+    init: function () {
+      var self = this;
+      if(!self.afterElement) {
+        return;
+      }
+      self._resizeListener();
+    },
+    _resizeListener: function () {
+      var self = this;
+      $(window).on('resize orientationchange load', function() {
+        // Regexp for removing quotes added by various browsers
+        self.currentBreakpoint = self.afterElement.getPropertyValue('content').replace(/^["']|["']$/g, '');
+        if (self.currentBreakpoint !== self.lastBreakpoint) {
+          console.log(self.currentBreakpoint);
+          $(window).trigger('breakpoint-change', self.currentBreakpoint);
+          self.lastBreakpoint = self.currentBreakpoint;
+        }
+      });
     }
-}
+  };
+
+  parent.mediaqueryListener = parent.mediaqueryListener || new MediaQueryListener();
+  return parent;
+
+}(RMC_media_check || {}, jQuery));
+
+$(window).on('breakpoint-change', function(e, breakpoint) {
+  if(breakpoint === 'sm') {
+    document.body.innerHTML = 'CSS Breakpoint <span>screen-small</span>';
+  }
+  if(breakpoint === 'md') {
+    document.body.innerHTML = 'CSS Breakpoint <span>screen-medium</span>';
+  }
+  if(breakpoint === 'lg') {
+    document.body.innerHTML = 'CSS Breakpoint <span>screen-large</span>';
+  }
+});
